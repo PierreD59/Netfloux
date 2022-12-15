cc2769c87387e7a10947c15cdfd6f337be8bee06
Robin Hotton <Robin.hotton@hotmail.com>
2022-12-15 15:28:06 +0100

    feat: scrollbar :sparkles:

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,12 @@
 import Categorie from "../layouts/categorie/Categorie";
 export default function HomePage() {
-  return <Categorie />;
+  return (
+    <>
+      <Categorie titreCategorie="Films pour vous" />
+      <Categorie titreCategorie="Séries pour vous" />
+      <Categorie titreCategorie="Musiques pour vous" />
+      <Categorie titreCategorie="Youtube pour vous" />
+      <Categorie titreCategorie="Démos de jeux vidéos disponibles" />
+    </>
+  );
 }
