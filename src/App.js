e55b3b550f7501fab83f7c1bd7eb8f051318d5ca
Robin Hotton <Robin.hotton@hotmail.com>
2022-12-16 12:56:38 +0100

    fix: hotfix conflit navbar/carousel :bug:

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <div className="App">
         {/* <HeaderLanding /> */}
         <Header />
-        {/* <HomePage /> */}
+        <HomePage />
         {/* <AfficherPage /> */}
       </div>
     </>
