4927ee17c01faaff116cc8ae889b4387a1b1f17e
PierreD59 <pierre.dmt59@gmail.com>
2022-12-16 15:05:01 +0100

    Ajout du composant Pub

---
+++ src/components/homePage/pub/Pub.jsx
@@ -0,0 +1,12 @@
+export default function Pub() {
+  return (
+    <>
+      <div className="pubBlock">
+        <img
+          src="https://media.istockphoto.com/id/1191001701/fr/photo/pop-corn-et-clapperboard.jpg?s=612x612&w=0&k=20&c=AHQ7hOMdCMRfAya18h3rznNEflqmFS3Q90UznAbNfzM="
+          alt="Photo d'une pub"
+        />
+      </div>
+    </>
+  );
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,20 @@
 import Categorie from "../components/homePage/categorie/Categorie";
 import MyApp from "../components/homePage/carousel/Carousel";
+import Pub from "../components/homePage/pub/Pub";
 export default function HomePage() {
   return (
     <>
       <MyApp />
       <Categorie titre="Contenu qui pourrait vous plaire" />
+      <Pub/>
       <Categorie titre="Films pour vous" />
       <Categorie titre="Séries pour vous" />
+      <Pub/>
       <Categorie titre="Musiques pour vous" />
       <Categorie titre="Youtube pour vous" />
+      <Pub/>
       <Categorie titre="Démos de jeux vidéos disponibles" />
+      <Pub/>
     </>
   );
 }
